ffa6ad353a5dd0270f7ec263c6d40b1090012f47
Or Gordin <orgordin@gmail.com>
2025-06-09 20:25:47 +0300

    Setup Jest and add initial component test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  testEnvironment: 'jsdom'
+};
